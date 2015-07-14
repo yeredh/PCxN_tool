2afea919fdda8e4cf90cfaa5aa1aca9181ba8529
yeredh <yered.h@gmail.com>
2015-07-14 13:02:20 -0400

    Shiny App for PCxN: GO

---
+++ PCxN04/global.R
@@ -0,0 +1,11 @@
+library(data.table)
+path.net.dframe <- data.table(readRDS("data/pathCor_GO_dframe.RDS"))
+path.net.dframe$PathCor <- round(path.net.dframe$PathCor,4)
+path.net.dframe$Overlap.Coefficient <- round(path.net.dframe$Overlap.Coefficient,4)
+
+
+# Get the names for all pathways
+path.names = unique(c(path.net.dframe$Pathway.A,path.net.dframe$Pathway.B))
+path.names <- sort(path.names)
+path.list <- as.list(path.names)
+names(path.list) <- path.names
